75825a6e45ac2e73424fde866c6c01501fd8222e
adarsh970 <69689205+adarsh970@users.noreply.github.com>
2021-12-13 18:06:41 +0530

    Update final project.py

--- final project.py
+++ final project.py
@@ -4,27 +4,27 @@ print("------------------------ RANDOM PASSWAORD GENERATOR ---------------------
 m=1
 
 while m==1:
-    strn="qwertyuiopasdfghjklzxcvbnmQAZWSXEDCRFVTGBYHNU?JMIKOLP1478520369@#$%&"
-    l2=[]
-    strn3=""
-    s1={}
+    strn="qwertyuiopasdfghjklzxcvbnmQAZWSXEDCRFVTGBYHNU?JMIKOLP1478520369@#$%&"             #string declaration
+    l2=[]                                                                                   #an empty list
+    strn3=""                                                                                #an empty string
+    s1={}                                                                                   #an empty set
     
     l1=[]
-    print("\n")
-    n=int(input("Enter the length of the passwaord : "))
-    s1= set(strn)
+    print("\n")                                                                             #to change the line
+    n=int(input("Enter the length of the passwaord : "))                            #tking input of the length of the new password
+    s1= set(strn)                                               #string Type converted to set
     for i in range (n):
         
 
         l1=list(s1)
     for i in range (n):
 
-        l2.append(l1[i])
+        l2.append(l1[i])                        #adding elements to the list
     for x in l2:
         strn3+=x
-    print("\n\nThe new generated password is : ",strn3)
+    print("\n\nThe new generated password is : ",strn3)  #to display new password
     print("\n\n")
     m+=1
-    m=int(input("Press 1 to change the length of the password and generate another password or press another key to exit : "))
+    m=int(input("Press 1 to change the length of the password and generate another password or press another key to exit : "))   #inputtng key value to decide the program to continue
     print("\n")
 print("                       ~~ ACTION TERMINATED ~~                            \n")
